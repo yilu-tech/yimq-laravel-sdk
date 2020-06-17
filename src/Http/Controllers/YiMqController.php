2becf0a6a3baee47e4a5b28442a062d1e702662f
lishu <lishu@yilu.co>
2020-06-17 23:25:14 +0800

    add yimq action log

--- src/Http/Controllers/YiMqController.php
+++ src/Http/Controllers/YiMqController.php
@@ -14,6 +14,9 @@ class YiMqController
     function run(Request $request,YiMqActor $yiMqActor,YiMqActorConfig $yiMqActorConfig,YiMqActorClear $yiMqActorClear){
         $action = $request->input('action');
         $context = $request->input('context');
+        if(env('YIMQ_ACTION_LOG',true)){
+            \Log::info("YiMQ Action $action",$context);
+        }
         switch ($action){
             case 'TRY':
                 return $yiMqActor->try($context);
