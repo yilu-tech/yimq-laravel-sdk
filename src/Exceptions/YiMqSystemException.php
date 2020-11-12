9678fe257082c2822edbacade3bed288b28879bb
lishu <lishu@yilu.co>
2020-11-12 09:50:40 +0800

    增加日志

--- src/Exceptions/YiMqSystemException.php
+++ src/Exceptions/YiMqSystemException.php
@@ -7,45 +7,23 @@ namespace YiluTech\YiMQ\Exceptions;
 use RuntimeException;
 use Psr\Log\LoggerInterface;
 use Illuminate\Contracts\Support\Responsable;
+use YiluTech\YiMQ\YiMqActor;
 
-class YiMqSystemException extends RuntimeException implements  Responsable
+class YiMqSystemException extends RuntimeException implements Responsable
 {
     protected $code = 500;
-    protected $response;
+    public $data;
     public function __construct($message = "",$data=null)
     {
-        $data = [
+        parent::__construct($message);
+        $this->data = [
             "message" => $message,
             "data" => $data,
-            "stack" => $this->getTrace()
         ];
-        array_unshift($data['stack'],[
-            "file" => $this->getFile(),
-            "line" => $this->getLine()
-        ]);
-        $this->response = response()->json($data,$this->getCode());
-        parent::__construct($message);
+        $this->response = response()->json($this->data,$this->getCode());
     }
 
-    public function report()
-    {
 
-
-        try {
-            $logger = app()->make(LoggerInterface::class);
-        } catch (Exception $ex) {
-            throw $this;
-        }
-
-        $logger->error(
-            $this->getMessage(),
-            ['exception' => $this]
-        );
-    }
-
-    /**
-     * @inheritDoc
-     */
     public function toResponse($request)
     {
         return $this->response;
